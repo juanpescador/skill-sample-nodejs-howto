aa02c4b163323dcc8b5e13893a944bafc853b802
John <juanrobertopescador@gmail.com>
2018-03-23 17:03:07 +0100

    Add missing food temperatures information

---
+++ lambda/custom/foodTemperatures.js
@@ -0,0 +1,24 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent']*/
+module.exports = {
+    'FOOD_TEMPERATURE_EN_GB': {
+        'reheat ham': '73.9 degrees Celsius.',
+        'poultry': '73.9 degrees Celsius',
+        'eggs': '71.1 degrees Celsius',
+        'fish': '62.8 degrees Celsius',
+        'leftovers': '73.9 degrees Celsius',
+        'casseroles': '73.9 degrees Celsius'
+    },
+    'FOOD_TEMPERATURE_EN_US': {
+        'beef': '145 degrees Fahrenheit and allow to rest for at least 3 minutes.',
+        'ground meats': '160 degrees Fahrenheit.',
+        'ham': '145 degrees Fahrenheit and allow to rest for at least 3 minutes.',
+        'reheat ham': '165 degrees Fahrenheit.',
+        'poultry': '165 degrees Fahrenheit',
+        'eggs': '160 degrees Fahrenheit',
+        'fish': '145 degrees Fahrenheit',
+        'leftovers': '165 degrees Fahrenheit',
+        'casseroles': '165 degrees Fahrenheit'
+    }
+};
