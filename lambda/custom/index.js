e1cb7b11ae4e8276774f3e047779ef1a8ca18a24
John <juanrobertopescador@gmail.com>
2018-03-23 16:44:10 +0100

    Repurpose sample skill for Cooking Temperature
    Add safe cooking temperatures for foods available at USDA website:
    https://www.fsis.usda.gov/wps/portal/fsis/topics/food-safety-education/get-answers/food-safety-fact-sheets/safe-food-handling/safe-minimum-internal-temperature-chart/ct_index

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,66 +1,45 @@
 /* eslint-disable  func-names */
 /* eslint quote-props: ["error", "consistent"]*/
 /**
- * This sample demonstrates a sample skill built with Amazon Alexa Skills nodejs
- * skill development kit.
- * This sample supports multiple languages (en-US, en-GB, de-GB).
- * The Intent Schema, Custom Slot and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-howto
+ * Based on the sample skill available at: https://github.com/alexa/skill-sample-nodejs-howto
  **/
 
 'use strict';
 
 const Alexa = require('alexa-sdk');
-const recipes = require('./recipes');
+const foodTemperatures = require('./foodTemperatures');
 
 const APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
 
 const languageStrings = {
     'en': {
         translation: {
-            RECIPES: recipes.RECIPE_EN_US,
+            FOOD_TEMPERATURES: foodTemperatures.FOOD_TEMPERATURE_EN_US,
             // TODO: Update these messages to customize.
-            SKILL_NAME: 'Minecraft Helper',
-            WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with?",
+            SKILL_NAME: 'Cooking Temperature',
+            WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the cooking temperature for a beef? ... Now, what can I help you with?",
             WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
-            DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
-            HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
-            HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            DISPLAY_CARD_TITLE: '%s  - Safe cooking temperature for %s.',
+            HELP_MESSAGE: "You can ask questions such as, what\'s the cooking temperature, or, you can say exit...Now, what can I help you with?",
+            HELP_REPROMPT: "You can say things like, what\'s the cooking temperature, or you can say exit...Now, what can I help you with?",
             STOP_MESSAGE: 'Goodbye!',
-            RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
-            RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
-            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
-            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
-            RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?',
+            FOOD_TEMPERATURE_REPEAT_MESSAGE: 'Try saying repeat.',
+            FOOD_TEMPERATURE_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
+            FOOD_TEMPERATURE_NOT_FOUND_WITH_ITEM_NAME: 'the cooking temperature for %s. ',
+            FOOD_TEMPERATURE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that food. ',
+            FOOD_TEMPERATURE_NOT_FOUND_REPROMPT: 'What else can I help with?',
         },
     },
     'en-US': {
         translation: {
-            RECIPES: recipes.RECIPE_EN_US,
-            SKILL_NAME: 'American Minecraft Helper',
+            FOOD_TEMPERATURES: foodTemperatures.FOOD_TEMPERATURE_EN_US,
+            SKILL_NAME: 'American Cooking Temperature',
         },
     },
     'en-GB': {
         translation: {
-            RECIPES: recipes.RECIPE_EN_GB,
-            SKILL_NAME: 'British Minecraft Helper',
-        },
-    },
-    'de': {
-        translation: {
-            RECIPES: recipes.RECIPE_DE_DE,
-            SKILL_NAME: 'Assistent für Minecraft in Deutsch',
-            WELCOME_MESSAGE: 'Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?',
-            WELCOME_REPROMPT: 'Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.',
-            DISPLAY_CARD_TITLE: '%s - Rezept für %s.',
-            HELP_MESSAGE: 'Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-            HELP_REPROMPT: 'Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-            STOP_MESSAGE: 'Auf Wiedersehen!',
-            RECIPE_REPEAT_MESSAGE: 'Sage einfach „Wiederholen“.',
-            RECIPE_NOT_FOUND_MESSAGE: 'Tut mir leid, ich kenne derzeit ',
-            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'das Rezept für %s nicht. ',
-            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'dieses Rezept nicht. ',
-            RECIPE_NOT_FOUND_REPROMPT: 'Womit kann ich dir sonst helfen?',
+            FOOD_TEMPERATURES: foodTemperatures.FOOD_TEMPERATURE_EN_GB,
+            SKILL_NAME: 'British Cooking Temperature',
         },
     },
 };
@@ -77,7 +56,7 @@ const handlers = {
         this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
         this.emit(':responseReady');
     },
-    'RecipeIntent': function () {
+    'FoodTemperatureIntent': function () {
         const itemSlot = this.event.request.intent.slots.Item;
         let itemName;
         if (itemSlot && itemSlot.value) {
@@ -85,23 +64,23 @@ const handlers = {
         }
 
         const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
-        const myRecipes = this.t('RECIPES');
-        const recipe = myRecipes[itemName];
+        const myfoodTemperatures = this.t('FOOD_TEMPERATURES');
+        const foodTemperature = myfoodTemperatures[itemName];
 
-        if (recipe) {
-            this.attributes.speechOutput = recipe;
-            this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
+        if (foodTemperature) {
+            this.attributes.speechOutput = foodTemperature;
+            this.attributes.repromptSpeech = this.t('FOOD_TEMPERATURE_REPEAT_MESSAGE');
 
-            this.response.speak(recipe).listen(this.attributes.repromptSpeech);
-            this.response.cardRenderer(cardTitle, recipe);
+            this.response.speak(foodTemperature).listen(this.attributes.repromptSpeech);
+            this.response.cardRenderer(cardTitle, foodTemperature);
             this.emit(':responseReady');
         } else {
-            let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
-            const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
+            let speechOutput = this.t('FOOD_TEMPERATURE_NOT_FOUND_MESSAGE');
+            const repromptSpeech = this.t('FOOD_TEMPERATURE_NOT_FOUND_REPROMPT');
             if (itemName) {
-                speechOutput += this.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
+                speechOutput += this.t('FOOD_TEMPERATURE_NOT_FOUND_WITH_ITEM_NAME', itemName);
             } else {
-                speechOutput += this.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
+                speechOutput += this.t('FOOD_TEMPERATURE_NOT_FOUND_WITHOUT_ITEM_NAME');
             }
             speechOutput += repromptSpeech;
 
